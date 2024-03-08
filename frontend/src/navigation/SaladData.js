a4bd0c8b0c49d85c15c50fbba9645ed65536e379
shiwang pandey <110737840+ShiwangPande@users.noreply.github.com>
2024-03-08 20:18:21 +0530

    added some photos

---
+++ frontend/src/navigation/SaladData.js
@@ -0,0 +1,50 @@
+const datas = {
+    saladItems: [
+        {
+            id: 1,
+            title: "Chickpea Salad",
+            description: "tomato sauce, mozzarella cheese, and basil.",
+            price: "300",
+            image: "https://i.postimg.cc/TPr9Nwvr/chickpea-salad-180x180.webp",
+            ingredient: "basil",
+        },
+        {
+            id: 2,
+            title: "Corn Pomegranate Salad",
+            description: "bell peppers, onions, mushrooms, and olives.",
+            price: "300",
+            image: "https://i.postimg.cc/fTrBJpv0/corn-pomegranate-salad-recipe-featured-180x180.webp",
+
+            ingredient1: "mushroom",
+        },
+        {
+            id: 3,
+            title: "Indian Onion Salad",
+            description: "tomato sauce, mozzarella cheese, and Pineapple",
+            price: "270",
+            image: "https://i.postimg.cc/cLtTYhDk/indian-onion-salad-featured-180x180.webp",
+            ingredient: "pineapple",
+        },
+        {
+            id: 4,
+            title: "Kala Chana Chaat Salad",
+            description: " tomato sauce, mozzarella cheese, and feta cheese.",
+            price: "250",
+            image: "https://i.postimg.cc/3wM1ZGV8/kala-chana-chaat-recipe-featured-180x180.webp",
+
+            ingredient: "cheese",
+        },
+        {
+            id: 5,
+            title: "Sprouted Moong Salad",
+            description: "tomato sauce, mozzarella cheese,sliced mushrooms.",
+            price: "300",
+            image: "https://i.postimg.cc/tTpzKj98/Sprouted-moong-salad-featured-180x180.webp",
+
+            ingredient: "mushroom",
+
+        },
+    ],
+}
+
+export default datas;
